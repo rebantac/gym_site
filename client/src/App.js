fd6e4e87756b992d8f44153407c251f1cab745b8
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2023-02-25 17:57:51 +0530

    manage conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
-import { Navbar, Footer } from './components';
+import { Footer } from './components';
 import { Home, Profile, GetStarted, LoginUser, LoginGymOwner, Dashboard  } from './pages'
 
 import './App.css';
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,7 @@
-import Navbar from "./Navbar";
 import Footer from "./Footer";
 import FormField from "./FormField";
 
 export {
-    Navbar,
     Footer,
     FormField,
 }

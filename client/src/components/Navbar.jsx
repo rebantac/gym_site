fd0db84599424024c86ba54e772b4a21ddcfb9a4
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2023-02-25 17:40:56 +0530

    add Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/dumbell.png'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav'>
+        <div >
+        <img className='logo' src={logo} alt='logo' height='50px' />
+        </div>
+      
+        <div>
+        <button className='btn'>Log In</button>
+       
+        </div>
+      
+    </div>
   )
 }
 

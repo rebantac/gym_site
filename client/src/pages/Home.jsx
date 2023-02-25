15adb7547479a49eb968caed3b381ba038580326
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2023-02-26 00:36:09 +0530

    add rebanta-7

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
-import UserNavbar from '../components/UserNavbar'
+import { GymCard, UserNavbar } from '../components';
+
+import data from '../assets/data.json';
 
 const Home = () => {
   return (
     <div>
       <UserNavbar />
 
-      
+      {data && data.map((element) => {
+        return (
+          <div>
+            <GymCard
+              id={element.id}
+              name={element.name}
+              gym_name={element.gym_name}
+              registrationFee={element.registrationFee}
+              monthlyFee={element.monthlyFee}
+            />
+          </div>
+        );
+      })}
     </div>
   )
 }

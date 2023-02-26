ef95e6e2db5f4ad3aa1eb44a6c96447d91247ed4
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2023-02-26 08:49:43 +0530

    add rebanta-8

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
 
           <Route path='/home' element={<Home />}/>
 
-          <Route path='/profile' element={<Profile />}/>
+          <Route path='/profile/:id' element={<Profile />}/>
 
           <Route path='/login-user' element={<LoginUser />}/>
 
--- client/src/components/UserNavbar.jsx
+++ client/src/components/UserNavbar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom";
 
-import logo from '../assets/dumbell.png'
+import logo from '../assets/logo.png'
 
 const UserNavbar = () => {
+  const id = "U001"; // later fetched from Home as a props
+
   return (
     <div >
       <div >
         <img className='logo' src={logo} alt='logo' height='50px' />
       </div>
 
-			<Link to='/profile'><button>PROFILE</button></Link>
+			<Link to={`/profile/${id}`}><button>PROFILE</button></Link>
 			<Link to=''><button>CONTACT US</button></Link>
 
 			<p>Name</p>

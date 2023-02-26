4e6779f6c9407463d4f1372e917a6bf0f4bae969
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2023-02-26 10:59:08 +0530

    add Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+
+import data from '../assets/data.json'
 
 const Dashboard = () => {
-  const [id, setId] = useState('')
+  const [id, setId] = useState('');
+  const [gymDetail, setGymDetail] = useState({});
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
-    setId(localStorage.getItem('gym_id'))
+    setId(localStorage.getItem('gym_id'));
+
+    const fetchGymData = async () => {
+      const gymDetailData = data.map((element) => {
+        if (element.id === id.id)
+          return element;   
+        console.log("Dashboard : ", id)
+        console.log("Element Id : ", element.id)     
+      });
+
+      setGymDetail(gymDetailData);
+      console.log(gymDetail)
+      setLoading(false)
+    }
+
+    console.log("Dashboard outside: ", id)
+    fetchGymData();
   }, [])
   
-  console.log(id)
 
   return (
     <div></div>
